c29f9bf095d15baf401d631ea781a12656e51ccd
Coinsinical <88478301+Coinsinical@users.noreply.github.com>
2021-12-13 16:22:39 +0800

    实现玩家直线运动

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #define WIDTH 1280 // 画面宽度
 #define HEIGHT 720 // 画面高度
 IMAGE im_box[15];//定义盒子图片
+int id = 0;
 
 struct Player //定义坐标结构体记录坐标
 {
@@ -90,10 +91,59 @@ void Start()
 	fillcircle(player.x, player.y, 10);//绘制玩家
 }
 
+void PlayerMove()
+{
+	//获取鼠标按下时长
+	ExMessage m;//记录鼠标动作
+	clock_t start_t, stop_t;//定义结构体获取鼠标按下与松开时间
+	double t;
+	while (1)
+	{
+		m = getmessage(EM_MOUSE);
+		if (m.message == WM_LBUTTONDOWN) //如果鼠标左键按下
+		{
+			start_t = clock(); // 获取鼠标按下时间 
+		}
+		if (m.message == WM_LBUTTONUP)//如果释放鼠标左键
+		{
+			stop_t = clock(); //获取鼠标释放时间
+			break; //跳出循环
+		}
+	}
+	t = (double)(stop_t - start_t);//计算鼠标按下时长
+
+	int i;
+	BeginBatchDraw();
+	for (i = 0;i < 50;i++)
+	{
+		cleardevice();
+		if (box[id].dir == 1)
+		{
+			player.x += sqrt(3) * t / 250;
+			player.y -= t / 250;
+		}
+		else if (box[id].dir == 0)
+		{
+			player.x -= sqrt(3) * t / 250;
+			player.y -= t / 250;
+		}
+		//绘制静止盒子
+		int j;
+		for (j = 19;j >= 0;j--)
+		{
+			putimagePng(box[j].c_x - 151.5, box[j].c_y - 90.3, &box[j].im_box);
+		}
+		fillcircle(player.x, player.y, 10);
+		FlushBatchDraw(); //批量绘制
+		Sleep(10);
+	}
+	EndBatchDraw(); //结束批量绘图
+}
 int main()
 {
 	srand((unsigned)time(0)); //产生随机数种子
 	Start();
+	PlayerMove();
 	_getch();
 	closegraph();
 	return 0;
